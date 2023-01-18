a2035b9b6425e7c8fb65ba793c5b94fcde7d3388
Igor Minayeu <igor.minayeu@gmail.com>
2023-01-18 14:16:59 +0300

    lesson 11-12

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSygZMPOdHR5PdI5ep1PWCERYy59Lxsu6EBUg&usqp=CAU"
+                alt="logo"/>
+        </header>
+    );
+};
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className={'content'}>
+            <div>
+                <img
+                    src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"
+                    alt="image"/>
+            </div>
+            <div>ava + description</div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+
+

0d4ce83f8cebc79a7867028d03633d50aa719857
Igor Minayeu <igor.minayeu@gmail.com>
2023-01-20 15:46:12 +0300

    lesson 21

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     return (
         <header className={s.header}>
             <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSygZMPOdHR5PdI5ep1PWCERYy59Lxsu6EBUg&usqp=CAU"
+                src="https://img.freepik.com/free-vector/flat-design-cookies-logo-template_23-2150053055.jpg?size=338&ext=jpg&ga=GA1.1.1737754170.1674217942"
                 alt="logo"/>
         </header>
     );

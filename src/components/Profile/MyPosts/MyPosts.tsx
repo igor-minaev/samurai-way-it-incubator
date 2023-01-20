66a5ac22633ed3d48078a748d54d62497ce9cc01
Igor Minayeu <igor.minayeu@gmail.com>
2023-01-20 12:21:51 +0300

    lesson 18

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -11,9 +11,8 @@ export const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message={'Hi, how are you?'} likeCounts={15}/>
+                <Post message={"It's my first post"} likeCounts={20}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import s from './Post.module.css'
 
-export const Post = () => {
+type PostType = {
+    message: string
+    likeCounts: number
+}
+
+export const Post = (props: PostType) => {
     return (
         <div className={s.item}>
             <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTApbxj4499GJJWMYvKUVnzMUBJBt1b_Aob0A&usqp=CAU"
                 alt="avatar"/>
-            Post 1
+            {props.message}
             <div>
-                <span>like</span>
+                <span>like {props.likeCounts}</span>
             </div>
         </div>
 
